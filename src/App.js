c1d7e8856836e3542387a7af21fd03c3d057f9cf
Ki-Sung <kcs4912@gmail.com>
2023-02-08 19:15:18 +0900

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Sidbar from "./components/sidebar/Sidbar";
 import Topbar from "./components/topbar/Topbar";
+import "./app.css"
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <div className="container">
+        <Sidbar/>
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
