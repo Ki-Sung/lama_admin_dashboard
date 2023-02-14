0ca79d813f413764c2c2ff5ddb457585adb06525
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-02-14 15:07:49 +0900

    user, edit user page

--- src/dumyData.js
+++ src/dumyData.js
@@ -49,3 +49,86 @@ export const userData = [
         "Active User": 3000,
     },
 ];
+
+export const userRows = [
+    { 
+        id: 1, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 2, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 3, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 4, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 5, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 6, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 7, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 8, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 9, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+    { 
+        id: 10, 
+        username: 'Jon Snow', 
+        avatar: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/af52add3-7fe8-4280-b502-603f1b2729e1/Screenshot_2023-02-14_at_10.47.38_AM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230214%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230214T020522Z&X-Amz-Expires=86400&X-Amz-Signature=e327d7f9fb6fffad861239fc66bd614b5c375974ca747d0bb9b71dda2e1b2a57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Screenshot%25202023-02-14%2520at%252010.47.38%2520AM.png%22&x-id=GetObject', 
+        email: 'jon@gmail.com', 
+        status: 'active', 
+        transaction: '$120.00'
+    },
+];
