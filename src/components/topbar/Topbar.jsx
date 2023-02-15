6b2311de094150d9f4dd057adeafb145210eaa6b
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-02-15 10:20:58 +0900

    replace code for avatar img

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -21,7 +21,7 @@ export default function Topbar() {
                     <div className='topbarIconContainer'>
                         <Settings />
                     </div>
-                    <img src='https://url.kr/v37mok' alt='' className='topAvatar'/>
+                    <img src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c18c4293-5763-4576-b0eb-30db36e230a7/main.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230215T011714Z&X-Amz-Expires=86400&X-Amz-Signature=a93cc15ff8791ae790a76d3e50541952d22c0a45055ae8a951350fdc0132fab8&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22main.jpeg%22&x-id=GetObject' alt='' className='topAvatar'/>
                 </div>
             </div>
         </div>
--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -8,7 +8,7 @@ export default function WidgetSm() {
             <ul className="widgetSmList">
                 <li className="widgetSmListItem">
                     <img
-                        src="https://url.kr/rk63ld"
+                        src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/69bed50e-36e9-4952-a8fc-42e377f4e2ae/sangyoon.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230215T011805Z&X-Amz-Expires=86400&X-Amz-Signature=4c4a25a90645ac9f93372bab40b2c603b4e82ef549cc372f2aa77e5ef8dfb5ed&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22sangyoon.jpeg%22&x-id=GetObject"
                         alt=""
                         className="widgetSmImg"
                     />
@@ -23,7 +23,7 @@ export default function WidgetSm() {
                 </li>
                 <li className="widgetSmListItem">
                     <img
-                        src="https://url.kr/48n26w"
+                        src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c18c4293-5763-4576-b0eb-30db36e230a7/main.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230215T011830Z&X-Amz-Expires=86400&X-Amz-Signature=d95ff0a659a57d63163922258e457e768b7643ed01cdeeef7ec5e1dadd681786&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22main.jpeg%22&x-id=GetObject"
                         alt=""
                         className="widgetSmImg"
                     />
@@ -38,7 +38,7 @@ export default function WidgetSm() {
                 </li>
                 <li className="widgetSmListItem">
                     <img
-                        src="https://url.kr/gj49ys"
+                        src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/3778285c-c7ec-45b1-9827-5c3cb09fda97/kisung.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230215T011846Z&X-Amz-Expires=86400&X-Amz-Signature=4db67004655cf2aa3abd9e92e2a675a40124594b88cedb8d01ace61d7afed6b3&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22kisung.jpeg%22&x-id=GetObject"
                         alt=""
                         className="widgetSmImg"
                     />
@@ -53,7 +53,7 @@ export default function WidgetSm() {
                 </li>
                 <li className="widgetSmListItem">
                     <img
-                        src="https://url.kr/of5wnt"
+                        src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/24dfc0bb-85fd-4b59-8ef5-1e02fa41208f/rayoung.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230215T011903Z&X-Amz-Expires=86400&X-Amz-Signature=fdc175f784c2d440e597a278676eac5368064b38a8e489332e6880c25e9fb3cf&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22rayoung.jpeg%22&x-id=GetObject"
                         alt=""
                         className="widgetSmImg"
                     />
@@ -68,7 +68,7 @@ export default function WidgetSm() {
                 </li>
                 <li className="widgetSmListItem">
                     <img
-                        src="https://url.kr/ygp73k"
+                        src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/78679ccc-d4ac-4795-97d3-8b6323ae3f69/jihyun.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230215T011919Z&X-Amz-Expires=86400&X-Amz-Signature=4100233a219dab9ae89fc127385be27aab4bbd8448d2ec671e7ebc0784cb45d9&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22jihyun.png%22&x-id=GetObject"
                         alt=""
                         className="widgetSmImg"
                     />
